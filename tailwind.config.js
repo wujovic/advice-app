5009848880bfdad9d141b5c18845988b5ba6ffb5
wujovic <veljkovujovic185@gmail.com>
2022-04-20 01:46:38 +0200

    API done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      adviceColor: "hsl(193, 38%, 86%)",
+      neon: "hsl(150, 100%, 66%)",
+      line: "hsl(217, 19%, 38%)",
+      cardBg: "hsl(217, 19%, 24%)",
+      bgColor: "hsl(218, 23%, 16%)",
+    },
     extend: {},
   },
   plugins: [],
