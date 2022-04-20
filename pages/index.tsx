97c279dc1b708d967aaa98fdfa19ddc45efaeaac
wujovic <veljkovujovic185@gmail.com>
2022-04-20 18:48:16 +0200

    styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import NewAdviceButton from '../components/NewAdviceButton'
+import Devider from '../components/Devider'
+import MobileDevider from '../components/MobileDevider'
 
 export const Home: NextPage = () => {
   const url = 'https://api.adviceslip.com/advice'
@@ -23,31 +26,25 @@ export const Home: NextPage = () => {
     <div className="h-[100vh] bg-bgColor">
       <Head>
         <title>adviso</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap');
-        </style>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap"
+        />
       </Head>
 
       <h1 className="text-neon">ADVICE #{adviceId}</h1>
       <h1 className="text-adviceColor">{advice}</h1>
 
-      <button className="mt-5 border py-2 px-5" onClick={fetchAdvice}>
-        New
-      </button>
+      <div className="md:hidden">
+        <MobileDevider />
+      </div>
+      <div className="hidden md:inline">
+        <Devider />
+      </div>
+
+      <NewAdviceButton fetchAdvice={fetchAdvice} />
     </div>
   )
 }
 
 export default Home
-
-// export async function getStaticProps() {
-//   const url = 'https://api.adviceslip.com/advice'
-//   const rawData = await axios.get(url)
-//   const newData = rawData.data
-
-//   return {
-//     props: { newData },
-//   }
-// }
